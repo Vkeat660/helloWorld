3665d862d12310fee789504c3474402dd638a0da
Vivian Keating <vkeat660@gmail.com>
2015-07-06 18:35:33 -0400

    Changed app title again lol

--- helloWorld/app/src/main/java/com/keating/vivian/helloworld/MainActivity.java
+++ helloWorld/app/src/main/java/com/keating/vivian/helloworld/MainActivity.java
@@ -2,8 +2,8 @@ package com.keating.vivian.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +11,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_main);
     }
 
